ead6f371ebd42b7f554ef2db51274bdd2e1434b8
Jhavagne Dunn <jhavagne.dunn@ibm.com>
2024-03-17 09:22:20 -0400

    implemented error handling using AOP

--- src/main/java/com/mycompany/propertymanagement/controller/UserController.java
+++ src/main/java/com/mycompany/propertymanagement/controller/UserController.java
@@ -2,7 +2,6 @@ package com.mycompany.propertymanagement.controller;
 
 import com.mycompany.propertymanagement.dto.UserDTO;
 import com.mycompany.propertymanagement.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
-
-    @Autowired
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO) {
         userDTO = userService.register(userDTO);
-        ResponseEntity<UserDTO> responseEntity = new ResponseEntity<>(userDTO, HttpStatus.CREATED);
-        return responseEntity;
+        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDTO) {
+        userDTO = userService.login(userDTO.getOwnerEmail(), userDTO.getPassword());
+        return new ResponseEntity<>(userDTO, HttpStatus.OK);
+
     }
 }
---
+++ src/main/java/com/mycompany/propertymanagement/exception/BusinessException.java
@@ -0,0 +1,17 @@
+package com.mycompany.propertymanagement.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class BusinessException extends RuntimeException{
+    private List<ErrorModel> errors;
+    public BusinessException(){}
+    public BusinessException(List<ErrorModel> errors){
+        this.errors = errors;
+    }
+
+}
