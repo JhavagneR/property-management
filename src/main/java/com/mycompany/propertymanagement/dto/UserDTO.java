5f248f9370085cc723d6f50e708a95feb3efeb86
Jhavagne Dunn <jhavagne.dunn@ibm.com>
2024-03-17 15:27:48 -0400

    implemented logging with logback and slf4j

--- src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
+++ src/main/java/com/mycompany/propertymanagement/dto/UserDTO.java
@@ -2,6 +2,9 @@ package com.mycompany.propertymanagement.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +15,13 @@ import lombok.Setter;
 public class UserDTO {
     private Long id;
     private String ownerName;
+    @NotNull(message = "Owner Email is mandatory")
+    @Size(min = 1, max = 50, message = "Owner Email should be between 1 to 50 characters in length")
+    @NotEmpty(message = "Owner Email cannot be empty")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
+
 }
